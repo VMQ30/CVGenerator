bebc870107e555a99176c311b79f29de087bf0a5
Vida <quiniavidamarie30@gmail.com>
2026-01-01 23:24:44 +0800

    created the navigation bar

--- cv-generator/src/components/Navbar.jsx
+++ cv-generator/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const navSections = [
   "Personal Details",
   "Work Experience",
@@ -8,10 +9,26 @@ const navSections = [
   "Export",
 ];
 
-function Button() {
-  return button;
+function Button({ label, onClick, isActive }) {
+  return (
+    <button onClick={onClick} className={isActive ? "selected" : ""}>
+      {label}
+    </button>
+  );
 }
 
 export function Navbar() {
-  return <nav></nav>;
+  const [currentSection, setCurrentSection] = useState(navSections[0]);
+  return (
+    <nav>
+      {navSections.map((section) => (
+        <Button
+          key={section}
+          label={section}
+          onClick={() => setCurrentSection(section)}
+          isActive={currentSection === section}
+        />
+      ))}
+    </nav>
+  );
 }
--- cv-generator/src/main.jsx
+++ cv-generator/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Navbar } from "./components/Navbar.jsx";
 import { PersonalDetails } from "./components/PersonalDetails.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Navbar />
     <PersonalDetails />
   </StrictMode>
 );
