efc038c1c76480f049719beca2b853f08c76e135
Vida <quiniavidamarie30@gmail.com>
2026-01-01 22:04:26 +0800

    Initialized the needed files

---
+++ cv-generator/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+  const navSections = [
+    "Personal Details",
+    "Work History",
+    "Education",
+    "Skills & Certifications",
+    "Interests",
+    "Additional",
+    "Export",
+  ];
+
+  return <nav></nav>;
+}
