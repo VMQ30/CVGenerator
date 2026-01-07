14cdf96499e9c60e292d57d28bdea3249db6c80f
Vida <quiniavidamarie30@gmail.com>
2026-01-07 18:48:06 +0800

    Work experience is now reflected in the live view

--- cv-generator/src/components/LiveVIew.jsx
+++ cv-generator/src/components/LiveVIew.jsx
@@ -1,18 +1,67 @@
-function RenderPersonalDetails({ personalData }) {
-  console.log(personalData);
+import "../styles/LiveView.css";
 
+function RenderPersonalDetails({ personalData }) {
   return (
     <div className="resume-personal-details">
       <h4 className="resume-name">{personalData.name}</h4>
       <div className="contact-details">
-        <p className="email">{personalData.email + "|"}</p>
-        <p className="contact-number">{personalData.contactNumber + "|"}</p>
-        <p className="location">{personalData.location}</p>
+        {personalData.email ? (
+          <>
+            <p className="email">{personalData.email}</p>
+            <p>|</p>
+          </>
+        ) : null}
+        {personalData.contactNumber ? (
+          <>
+            <p className="contact-number">{personalData.contactNumber}</p>
+            <p>|</p>
+          </>
+        ) : null}
+        {personalData.location ? (
+          <p className="location">{personalData.location}</p>
+        ) : null}
 
         {personalData.portfolio ? (
-          <p className="portfolio">{" | " + personalData.portfolio}</p>
+          <>
+            <p>|</p>
+            <p className="portfolio">{personalData.portfolio}</p>
+          </>
         ) : null}
       </div>
+    </div>
+  );
+}
+
+function RenderWorkExperience({ workData }) {
+  console.log(workData);
+
+  if (workData.length === 0) return null;
+
+  return (
+    <div className="resume-work-experience">
+      <h4>WORK EXPERIENCE</h4>
+      {workData.map((work) => (
+        <div className="resume-list" key={work.id}>
+          <div className="resume-header">
+            <h5 className="company-name">{work.companyName}</h5>
+            <h5 className="company-date">{`${work.startDate} - ${workData.endDate}`}</h5>
+          </div>
+
+          <div className="resume-details">
+            <div className="resume-details-header">
+              <p className="title">{work.jobTitle}</p>
+              <p className="location">{work.jobLocation}</p>
+            </div>
+            <div className="resume-bullets">
+              <ul>
+                {work.bullets.map((bullet, index) => (
+                  <li key={index}>{bullet}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
@@ -23,6 +72,7 @@ export function LiveView({ resumeData }) {
     <div className="live-view-wrapper">
       <div className="live-view-page">
         <RenderPersonalDetails personalData={resumeData.personalDetails} />
+        <RenderWorkExperience workData={resumeData.workExperience} />
       </div>
     </div>
   );
